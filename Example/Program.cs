c04686b5e9d5c6627106532246c180542efe1f0d
hexafluoride <xafluoride@gmail.com>
2018-01-08 20:18:48 +0300

    Initial commit

---
+++ Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Exchange;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinanceInstance binance = new BinanceInstance();
+            binance.PopulatePairGraph("./binance-pairs.txt");
+            binance.Connect();
+
+            while (!binance.TickerData.Any())
+                Thread.Sleep(500);
+
+            Console.WriteLine("Got ticker data");
+
+            while(true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(binance.TickerData[new Ticker("BTC", "USD")]);
+            }
+        }
+    }
+}
